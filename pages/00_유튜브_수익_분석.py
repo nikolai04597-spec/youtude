98a1171dd0639975047da0cc6a93a17ae612f0ce
nikolai04597-spec <nikolai04597@gmail.com>
2026-06-26 09:38:56 +0900

    Update 00_유튜브_수익_분석.py

--- pages/00_유튜브_수익_분석.py
+++ pages/00_유튜브_수익_분석.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # ==========================
 # 한글 폰트 설정
 # ==========================
-font_path = "NanumGothic.ttf"
+font_path = "../NanumGothic.ttf"
 
 font_manager.fontManager.addfont(font_path)
 
@@ -29,11 +29,10 @@ plt.rcParams['axes.unicode_minus'] = False
 # ==========================
 # 제목
 # ==========================
-st.title("💰 유튜브 예상 수익 분석기")
+st.title("💰 유튜브 수익 분석기")
 
 st.markdown("""
 유튜브 조회수를 기반으로 예상 광고 수익을 계산합니다.
 
 ※ 실제 수익은 국가, 시청자 특성, 광고 유형 등에 따라 달라질 수 있습니다.
 """)
-
